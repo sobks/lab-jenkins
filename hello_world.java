d23868667e32422eb29bb339d6ee59c5d048dd05
sobks <96662997+sobks@users.noreply.github.com>
2023-12-02 20:46:31 +0100

    Update hello_world.java

--- hello_world.java
+++ hello_world.java
@@ -1,11 +1,11 @@
-public class HelloWorld {
+public class hello_world {
 
     public String getMessage() {
         return "Salut, c’est notre première image Docker!!";
     }
 
     public static void main(String[] args) {
-        HelloWorld helloWorld = new HelloWorld();
+        hello_world helloWorld = new hello_world();
         System.out.println(helloWorld.getMessage());
     }
 }
